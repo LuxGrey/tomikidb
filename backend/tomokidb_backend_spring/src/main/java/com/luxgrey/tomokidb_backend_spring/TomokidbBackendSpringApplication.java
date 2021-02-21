b9a19b562a2655bb9007d07cb4320bb1957e8db8
LuxGrey <>
2021-02-21 18:16:45 +0100

    Created tomokidb_backend_spring with spring initializr

---
+++ backend/tomokidb_backend_spring/src/main/java/com/luxgrey/tomokidb_backend_spring/TomokidbBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.luxgrey.tomokidb_backend_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomokidbBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomokidbBackendSpringApplication.class, args);
+	}
+
+}
---
+++ backend/tomokidb_backend_spring/src/test/java/com/luxgrey/tomokidb_backend_spring/TomokidbBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxgrey.tomokidb_backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomokidbBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
